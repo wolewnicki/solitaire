bd31a4c2953e8aa34b90aa89cb836ff014cbdd45
William Olewnicki <wolewnicki@yahoo.com>
2020-05-12 22:32:58 -0400

    added dynamic card color

--- src/app/draw-card/draw-card.component.ts
+++ src/app/draw-card/draw-card.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrawCardComponent implements OnInit {
 
+  classObject = {}
+
   constructor() { }
 
   ngOnInit(): void {
@@ -15,17 +17,22 @@ export class DrawCardComponent implements OnInit {
   generateCard() {
     const suitIndex = Math.floor(Math.random() * this.suits.length)
     const rankIndex = Math.floor(Math.random() * this.ranks.length)
-    const suit = this.suits[suitIndex] 
-    const rank = this.ranks[rankIndex]
-    const assembledListItem = {suit: `${suit}`, rank: `${rank}`}
+    const randomSuit = this.suits[suitIndex] 
+    const randomRank = this.ranks[rankIndex]
+    const assembledListItem = {suit: `${randomSuit}`, rank: `${randomRank}`, color: this.suitColors[randomSuit]}
     this.cards.push(assembledListItem)
   }
 
   suits: string[] = ['♥','♦','♠','♣']
   ranks: any[] = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-
-  cards = [
-    { suit: '♥', rank: 'K'},
+  suitColors = {
+  '♥' : 'red',
+  '♦' : 'red',
+  '♠' : 'black',
+  '♣' : 'black'
+}
+    cards = [
+    { suit: '♥', rank: 'K', color: this.suitColors['♥']},
   ]
 
 }
